cf18bb9397ce7024a7d844a17d85cb1e8b89eb61
abhaypat <abhaypat@usc.edu>
2018-12-02 03:52:35 -0800

    updating parser

--- parser.py
+++ parser.py
@@ -1,29 +1,40 @@
 class network:
     def __init__(self):
-        self.nodes = list();
-        self.nodeNum = 0;
+        self.PI = list()
+        self.PO = list()
+        self.nodes = dict()
+        self.nodeNum = 0
         
     def insertNodes(self, node):
-        self.nodes.append(node)
-        if(node.nodeType == "const"):
+        self.nodes[node.name] = node
+        if(node.nodeType != "const"):
             self.nodeNum = self.nodeNum + 1;
+            
     
     def printNodes(self):
-        for node in self.nodes:
-            print (node.name+" "+str(node.Fin)+" "+str(node.Fout))
+        keys = self.nodes.keys()
+        keys.sort()
+        for node in keys:
+            print (str(self.nodes[node].name)+" "+str(self.nodes[node].Fin)+" "+str(self.nodes[node].Fout))
+        return
             
     def exists(self, searchNode):
-        for node in self.nodes:
-            if(node.name == searchNode):
-                return True, node
-        return False , None
+        if self.nodes.has_key(int(searchNode)):
+            return True, self.nodes[int(searchNode)]
+        return False , None              
+        
         
     def numberOfNodes(self):
         print self.nodeNum;
+        return
+        
+    def getNode(self, name):
+        if self.nodes.has_key(int(name)):
+            return self.nodes[int(name)]
         
 class node:
     def __init__(self, name, nodeType):
-        self.name = name
+        self.name = int(name)
         self.nodeType = nodeType;
         self.Fin = list()
         self.Fout = list()
@@ -42,38 +53,43 @@ class node:
     def setLevel(self, level):
         self.level = level
         
-file = open("networkOut.out",'r')
-
-pNtk = network()
-
-for line in file:
-    if "Primary" in line:
-        Id = line.split(':')[1].strip().split(' ')
-        for name in Id:
-            if "input" in line:
-                newNode = node(name, "Input")
-            else:
-                newNode = node(name, "Output")
-                
-            pNtk.insertNodes(newNode)
+        
+if __name__ == '__main__':        
+    file = open("networkOut.out",'r')
     
-    else:
-        nodeDet = line.strip().split(':')
-        if nodeDet[0] == "0":
-            continue
+    pNtk = network()
+    
+    for line in file:
+        if "Primary" in line:
+            Id = line.split(':')[1].strip().split(' ')
+            for name in Id:
+                if "input" in line:
+                    pNtk.PI.append(name)
+                    newNode = node(name, "Input")
+                else:
+                    pNtk.PO.append(name)
+                    newNode = node(name, "Output")
+                    
+                pNtk.insertNodes(newNode)
+        
         else:
-            (exist, tempNode) = pNtk.exists(nodeDet[0])
-            if not exist:
-                tempNode = node(nodeDet[0], "AND")
-                
-            if nodeDet[1] != '':
-                for fin in nodeDet[1].strip().split(' '):
-                    tempNode.insertFin([fin.split('-')[0], fin.split('-')[1]])
-
-            if nodeDet[2] != '':
-                for fout in nodeDet[2].strip().split(' '):
-                    tempNode.insertFout(fout.split('-')[0])
-            if not exist:
-                pNtk.insertNodes(tempNode)
-               
-print(pNtk.printNodes())
+            nodeDet = line.strip().split(':')
+            if nodeDet[0] == "0":
+                continue
+            else:
+                (exist, tempNode) = pNtk.exists(nodeDet[0])
+                if not exist:
+                    tempNode = node(nodeDet[0], "AND")
+                    
+                if nodeDet[1] != '':
+                    for fin in nodeDet[1].strip().split(' '):
+                        tempNode.insertFin([fin.split('-')[0], fin.split('-')[1]])
+    
+                if nodeDet[2] != '':
+                    for fout in nodeDet[2].strip().split(' '):
+                        tempNode.insertFout(fout.split('-')[0])
+                if not exist:
+                    pNtk.insertNodes(tempNode)
+    file.close()              
+    pNtk.printNodes()
+    #pNtk.numberOfNodes()
