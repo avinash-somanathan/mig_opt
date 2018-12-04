d94f90a6f68e27e0ffa776ad8b9c5ca1c3a2bebc
abhaypat <abhaypat@usc.edu>
2018-12-03 21:09:53 -0800

    updating parser

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ Created on Mon Dec  3 00:03:59 2018
 
 @author: abhay
 """
-import parser
+import parserNetwork
 import MIG
 
 
-parser.parser("networkOut.out")
+parserNetwork.parser("networkOut.out")
 
-MIG.convToMIG(parser.pNtk)
-parser.pNtk.printNodes()
+MIG.convToMIG(parserNetwork.pNtk)
+parserNetwork.pNtk.printNodes()
 
 
 
