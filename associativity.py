c57d9809e0d3e82ac0ea9f56287715c645f2c143
abhaypat <abhaypat@usc.edu>
2018-12-07 04:52:38 -0800

    associativity

--- associativity.py
+++ associativity.py
@@ -19,8 +19,8 @@ def swap(net1, net2, net1TopNode, net2TopNode):
 def associativity(cNetwork, node):
 	#pdb.set_trace()
 	fins = [x for x in [node.Fin[y] for y in range(len(node.Fin))]]
-	choice = [y for y in range(len(fins)) if (fins[y][0].nodeType == "MIG")]
-	#pdb.set_trace()
+	choice = [y for y in range(len(fins)) if (fins[y][0].nodeType == "AND")]
+	pdb.set_trace()
 	#choice gives the index of the fanins which has Maj node 
 
 	applyNode = None
@@ -32,17 +32,18 @@ def associativity(cNetwork, node):
 			if other!=fins[everyMaj]:
 				#pdb.set_trace()
 				if other in nextFins:
-					applyNode = fins[everyMaj][0]
-					commonNode = other[0]
+					applyNode = fins[everyMaj]
+					commonNode = other
 					break
-			if(applyNode != None):
-				break
+		if(applyNode != None):
+			break
 
 	if applyNode != None:
-		nextFins = applyNode[0].Fins
-		swapChoice = [x for x in nextFins if x!=applyNode]
+		nextFins = applyNode[0].Fin
+		swapChoice = [x for x in nextFins if x!=commonNode]
 		curFin = [x for x in fins if (x!=applyNode or x!=commonNode)][0]
-		swap(swapChoice[0], curFin, applyNode, node)
+		pdb.set_trace()
+		swap(swapChoice[0], curFin, applyNode[0], node)
 
 
 	return cNetwork
