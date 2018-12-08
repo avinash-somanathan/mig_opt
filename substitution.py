fce04f15d10641e9eff7e4414aa2f42707d18386
avinash-somanathan <somanata@usc.edu>
2018-12-08 12:54:26 -0800

    Making a few changes t dup

--- substitution.py
+++ substitution.py
@@ -4,36 +4,107 @@
 
 
 from random import randrange
+import dataStructure
+import parserNetwork
+import pdb
+from graphviz import Graph
 
-def Substitution(self, network, node)
+adjacency=dict()
+
+
+def Substitution(network, node):
 	[u,v] = pickPI(network, node)
-	duplicate_net(network, node)
-	newNode = createNode(network.nodeNum*2+1, "MIG", node.level+1)
+	newTop = duplicate_net_start(network, node)
+	pdb.set_trace()
+	newNodeTop = dataStructure.createNode(network.nodeNum+3, "MIG", node.level+2)
+	newNodeLeft = dataStructure.createNode(network.nodeNum+2, "MIG", node.level+1)
+	newNodeRight = dataStructure.createNode(network.nodeNum+1, "MIG", node.level+1)
+	newNodeLeft.insertFin([[node,0], u , [v[0],inv(v[1])]])
+	newNodeRight.insertFin([[newTop,0], [u[0],inv(u[1])] , [v[0],inv(v[1])]])
+	newNodeTop.insertFin([[newNodeRight,0],[newNodeLeft,0],v])
+	newNodeLeft.insertFout(newNodeTop)
+	newNodeRight.insertFout(newNodeTop)
+	network.insertNodes(newNodeRight)
+	network.insertNodes(newNodeTop)
+	network.insertNodes(newNodeLeft)
+	print newNodeTop,newNodeTop,newNodeLeft
+	pdb.set_trace()
 	recursive_replace(node, network, u, v)
-	recursive_replace(newDupNode, newNet, [u[0],inv(u[1])])
-	newNode.insertFin(
-	newNode.Fin[1] = v
-	newNode.Fin[2] = newNet
+	recursive_replace(newTop, network, [u[0],inv(u[1])], v)
+	
 
+	
 
-def pickPI(self, network, node)
-	rand_index = randrange(len(network.PI))
-	u = netowrk.PI[rand_index]			#pick a randm input
-	v = network.PI[rand_index+1]			#use circular feature in python
+def inv(i):
+	if i=='0':
+		return '1'
+	else:
+		return '0'
+
+def pickPI( network, node):
+	rand_index = randrange(len(network.PI)-1)
+	u = [network.getNode(network.PI[rand_index]),0]			#pick a randm input
+	v = [network.getNode(network.PI[rand_index+1]),0]			#use circular feature in python
+	pdb.set_trace()
 	return [u,v]
+def duplicate_net_start(network,node):
+	nodeNumold=network.nodeNum
+	newNode = dataStructure.createNode(nodeNumold + node.name, "MIG", node.level)
+	newNodeTopm1 = duplicate_net(network,node)
+	for f in node.Fin:
+		newNode.insertFin([network.getNode(f[0].name+nodeNumold),f[1]])
+	return newNode
 
-def duplicate_net(self, network, node)   #need to complete this def
+
+def duplicate_net( network, node):   #need to complete this def
+	nodeNumold=network.nodeNum
 	for i,fin in enumerate(node.Fin):
-		if fin.nodeType != "Input"
-			newNode = createNode(network.nodeNum + fin.name, "MIG", fin.level)
-			duplicate_net(network, fin)
-		for f in fin.Fin:
-			newNode.insertFin(self.getNode(f.name+network.nodeNum))
-			
-def recursive_replace(self, node, network, u, v)
+		if fin[0].nodeType != "Input":
+			newNode = dataStructure.createNode(nodeNumold + fin[0].name, "MIG", fin[0].level)
+			network.insertNodes(newNode)
+			duplicate_net(network, fin[0])
+			for f in fin[0].Fin:
+				newNode.insertFin([network.getNode(f[0].name+nodeNumold),f[1]])
+			for fo in fin[0].Fout:
+				newNode.insertFout(network.getNode(fo.name+nodeNumold))
+			return newNode
+def recursive_replace( node, network, u, v):
 	for fin in node.Fin:
-		if fin[0].name == u[0].name and fin[1] == u[1]:
-			fin[0].name = v.name
-			if fin.nodeType != "Input"
-				recursive_replace(self, fin, u, v)
+		pdb.set_trace()
+		if fin[0].name == v[0].name and int(fin[1]) == v[1]:
+			fin[0].name = u[0].name
+			if fin[0].nodeType != "Input":
+				recursive_replace( fin, network, u, v)
+
+def create_adjacency(network):
+	pi = list()
+	po = list()
+	for node in network.nodes.keys():
+        	edges = list()
+        	for i in range(0,len(network.nodes[node].Fout)):
+                	edges.append(network.nodes[node].Fout[i].name)
+	        if network.nodes[node].nodeType == "Input":
+        		pi.append(network.nodes[node].name)
+		if network.nodes[node].nodeType == "Output":
+            		po.append(network.nodes[node].name)
+	        adjacency[node] = edges
+	return pi,po,adjacency
+
+
+def draw_graph(adjacency):
+	g = Graph('G',filename = 'Boolean_Network')
+	for node in adjacency:
+		for Fout in adjacency[node]:
+            #print str(node) + " " + str(Fout)
+			g.edge(str(node),str(Fout))
+	g.view()
+
+
+
+parserNetwork.parser("networkTest2.out")
+Substitution(parserNetwork.pNtk,parserNetwork.pNtk.getNode(9))
 			
+parserNetwork.pNtk.printNodes()
+
+#pi,po,adj=create_adjacency(parserNetwork.pNtk)
+#draw_graph(adj)
